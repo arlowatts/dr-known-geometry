079ce70f21faeac3cef4a803782bedb4da06f4ef
Arlo Watts <arlowatts1@gmail.com>
2025-03-20 11:28:26 -0700

    Implement reliable pose estimation

--- testing/pose_estimation/pose_estimation.py
+++ testing/pose_estimation/pose_estimation.py
@@ -1,17 +1,32 @@
 import drjit as dr
 import mitsuba as mi
 import matplotlib.pyplot as plt
-import random
-import math
+import numpy as np
+import scipy.ndimage as ndimage
 
-model_file_name = 'model/bunny.obj'
+# set a mitsuba variant that supports differentiation
+mi.set_variant('llvm_ad_mono')
+
+# define optimization parameters
+model_path = 'model/bunny.obj'
+img_ref_path = 'model/ref_01_cropped.png'
 iteration_count = 100
+attempts_count = 100
 
-# Set a mitsuba variant that supports differentiation
-mi.set_variant('llvm_ad_rgb')
+# define scene parameters
+fov = 60
+img_size = 64
+sensor_distance = 2
+min_translation = mi.Point3f(-1.0, -1.0, -1.0)
+max_translation = mi.Point3f(1.0, 1.0, 1.0)
 
-# Select the direct_projective integrator to properly differentiate visibility
-# discontinuities while optimizing the object position and orientation
+# define the threshold for converting a floating-point image to a binary image
+binary_threshold = 0.1
+
+# initialize a random generator
+rng = np.random.default_rng()
+
+# use direct_projective integrator to differentiate visibility discontinuities
 integrator = {
     'type': 'direct_projective',
     'sppc': 2,
@@ -19,30 +34,30 @@ integrator = {
     'sppi': 0, # no indirect samples due to the simplicity of the scene
 }
 
-# Set the custom scene
+# load the scene
 scene = mi.load_dict({
     'type': 'scene',
     'integrator': integrator,
     'sensor': {
         'type': 'perspective',
-        'to_world': mi.scalar_rgb.Transform4f().look_at(origin=(0, 0, 2), target=(0, 0, 0), up=(0, 1, 0)),
-        'fov': 60,
+        'to_world': mi.scalar_rgb.Transform4f().translate((0.0, 0.0, -sensor_distance)),
+        'fov': fov,
         'film': {
             'type': 'hdrfilm',
-            'width': 64,
-            'height': 64,
+            'width': img_size,
+            'height': img_size,
             'pixel_format': 'luminance',
             'sample_border': True,
         },
     },
     'model': {
         'type': 'obj',
-        'filename': model_file_name,
-        'to_world': mi.scalar_rgb.Transform4f().translate((0.0, -0.75, 0.0)).scale(12.0),
+        'filename': model_path,
+        'to_world': mi.scalar_rgb.Transform4f(),
         'bsdf': {
             'type': 'diffuse',
             'reflectance': {
-                'type': 'rgb',
+                'type': 'uniform',
                 'value': 0.0,
             },
         },
@@ -50,107 +65,191 @@ scene = mi.load_dict({
     'light': {
         'type': 'constant',
         'radiance': {
-            'type': 'rgb',
+            'type': 'uniform',
             'value': 1.0,
         },
     },
 })
 
 def main():
-    # Access the mesh vertex positions
+    # access the mesh vertex positions
     params = mi.traverse(scene)
     vertex_positions = dr.unravel(mi.Point3f, params['model.vertex_positions'])
 
-    # Initialize the optimizer
-    opt = init_opt()
+    # load the reference image and compute its center of mass and orientation
+    img_ref = get_img_ref()
+    img_ref_binary = img_ref > binary_threshold
+    img_ref_border = ndimage.binary_dilation(img_ref_binary) ^ img_ref_binary
+    img_ref_cm = np.array(ndimage.center_of_mass(img_ref_binary))
+    img_ref_rot = np.array(ndimage.center_of_mass(img_ref_border)) - img_ref_cm
+    img_ref_sum = np.sum(img_ref_binary)
 
-    # Render the initial image
-    img_init = mi.render(scene)
+    # track the transform that most closely matches the reference image
+    best_quaternion = get_random_rotation()
+    best_translation = mi.Point3f(0, 0, 0)
+    best_loss = np.inf
 
-    # Randomly rotate the model and render the reference image
-    apply_random_transformation(params, vertex_positions)
-    img_ref = mi.render(scene)
+    # test random orientations and pick the best one for optimization
+    for attempt in range(attempts_count):
 
-    # Keep a histogram of the loss function to visualize the optimization process
+        # apply a random rotation
+        quaternion = get_random_rotation()
+        translation = mi.Point3f(0, 0, 0)
+        apply_transform(params, vertex_positions, quaternion, translation)
+
+        # render the image and compute its center of mass and orientation
+        img = 1 - mi.render(scene, params, seed=attempt)[:, :, 0]
+        img_binary = img > binary_threshold
+        img_border = ndimage.binary_dilation(img_binary) ^ img_binary
+        img_cm = np.array(ndimage.center_of_mass(img_binary))
+        img_rot = np.array(ndimage.center_of_mass(img_border)) - img_cm
+        img_sum = np.sum(img_binary)
+
+        # compute the pixel shift, scale, and rotation to make the images match
+        shift = img_ref_cm - img_cm
+        scale = np.sqrt(img_sum / img_ref_sum)
+        rotation = np.arctan2(img_rot[0] * img_ref_rot[1] - img_rot[1] * img_ref_rot[0], img_rot[0] * img_ref_rot[0] + img_rot[1] * img_ref_rot[1])
+
+        # compute the translation along the z-axis to set an appropriate scale
+        translation[2] = float(sensor_distance * scale - sensor_distance)
+
+        # compute the in-plane translation to match the reference image's center of mass
+        shift_scale = 1.0 / ((img_size * 0.5) * np.tan(np.deg2rad(fov * 0.5)) * (sensor_distance * scale))
+        translation[0] = float(-shift[1] * shift_scale)
+        translation[1] = float(-shift[0] * shift_scale)
+
+        # compute rotation quaternion for the in-plane rotation
+        quaternion = dr.auto.ad.Quaternion4f(0.0, 0.0, float(-np.sin(rotation * 0.5)), float(np.cos(rotation * 0.5))) * quaternion
+
+        # render the image again with the new transform
+        apply_transform(params, vertex_positions, quaternion, translation)
+        img_shifted = 1 - mi.render(scene, seed=attempt)[:, :, 0]
+
+        # compute the loss
+        loss_value = np.mean(np.square(img_shifted - img_ref))
+
+        # compare the loss to the previous best loss
+        if loss_value < best_loss:
+            best_quaternion = quaternion
+            best_translation = translation
+            best_loss = loss_value
+
+        print(f'Attempt {attempt+1:03d}: error={best_loss:08f}', end='\r')
+
+    print()
+
+    # initialize the optimizer
+    opt = init_opt(best_quaternion, best_translation)
+
+    # render the initial image
+    apply_optimized_transform(params, vertex_positions, opt)
+    img_init = 1 - mi.render(scene)[:, :, 0]
+
+    # keep a histogram of the loss function to visualize the optimization process
     loss_hist = []
 
-    for i in range(iteration_count):
+    for iteration in range(iteration_count):
 
-        # Apply the mesh transformation
-        apply_transformation(params, opt, vertex_positions)
+        # apply the mesh transformation
+        constrain_opt(opt)
+        apply_optimized_transform(params, vertex_positions, opt)
 
-        # Render the image
-        img = mi.render(scene, params, seed=i)
-
-        # Evaluate the loss function
+        # render the image and compute the loss
+        img = 1 - mi.render(scene, params, seed=iteration)[:, :, 0]
         loss = dr.mean(dr.square(img - img_ref))
+        loss_value = loss.array[0]
 
-        # Backpropagate through the rendering and take a gradient descent step
+        # backpropagate through the rendering and take a gradient descent step
         dr.backward(loss)
         opt.step()
 
-        # Log optimization progress
-        loss_hist.append(loss.array[0])
-        loss_value = loss.array[0]
+        # log the optimization progress
+        loss_hist.append(loss_value)
 
-        print(f'Iteration {i:02d}: error={loss_value:8f}', end='\r')
+        print(f'Iteration {iteration+1:03d}: error={loss_value:08f}', end='\r')
 
     print()
 
     display_results(loss_hist, img_init, img, img_ref)
 
-# Initialize the optimizer with a high learning rate to intentionally skip
-# around at the beginning to reach a global minimum
-def init_opt():
-    opt = mi.ad.Adam(lr=0.1)
+# load the reference image as a two-dimensional tensor
+def get_img_ref():
+    img_ref = mi.Bitmap(img_ref_path)
 
-    # Initialize the rotation quaternion
-    opt['rotation'] = dr.auto.ad.Quaternion4f(0, 0, 0, 1)
+    # convert the image to single-channel floating-point and resample
+    img_ref = img_ref.convert(pixel_format=mi.Bitmap.PixelFormat.Y, component_format=mi.Struct.Type.Float32)
+    img_ref = img_ref.resample([img_size, img_size])
+
+    # convert the image to a two-dimensional tensor
+    img_ref = mi.TensorXf(img_ref)[:, :, 0]
+
+    return img_ref
+
+# initialize the Adam optimizer
+def init_opt(quaternion, translation):
+    opt = mi.ad.Adam(lr=0.025)
+
+    opt['rotation'] = quaternion
+    opt['translation'] = translation
+
+    constrain_opt(opt)
 
     return opt
 
-# Randomly rotate the vertices and update the parameters
-def apply_random_transformation(params, vertex_positions):
-
-    # Choose a random rotation as an axis and an angle
-    axis = mi.warp.square_to_uniform_sphere((random.random(), random.random()))
-    angle = random.random() * 360
-
-    transform = mi.Transform4f().rotate(axis, angle)
-
-    params['model.vertex_positions'] = dr.ravel(transform @ vertex_positions)
-    params.update()
-
-# Apply the transformation given in the optimizer to the vertex positions
-def apply_transformation(params, opt, vertex_positions):
+# adjust the optimized parameters to satisfy reasonable constraints
+def constrain_opt(opt):
     opt['rotation'] = dr.normalize(opt['rotation'])
+    opt['translation'] = dr.clip(opt['translation'], min_translation, max_translation)
 
-    transform = mi.Transform4f(dr.quat_to_matrix(opt['rotation']))
+# generate a random rotation quaternion
+def get_random_rotation():
+
+    # uniformly choose a random axis and a random angle
+    axis = rng.standard_normal(3)
+    axis = axis / np.linalg.norm(axis)
+    angle = rng.uniform(0.0, 2.0 * np.pi)
+
+    # convert the axis and angle to a quaternion
+    axis = axis * np.sin(angle * 0.5)
+    quaternion = dr.auto.ad.Quaternion4f(float(axis[0]), float(axis[1]), float(axis[2]), float(np.cos(angle * 0.5)))
+
+    return quaternion
+
+# apply a rotation quaternion and translation to vertex positions
+def apply_transform(params, vertex_positions, quaternion, translation):
+    rotation_matrix = mi.Transform4f(dr.quat_to_matrix(quaternion))
+    translation_matrix = mi.Transform4f().translate(translation)
+
+    transform = translation_matrix @ rotation_matrix
 
     params['model.vertex_positions'] = dr.ravel(transform @ vertex_positions)
     params.update()
 
-# Display a histogram and three images in a figure
+# apply the transform given in the optimizer to vertex positions
+def apply_optimized_transform(params, vertex_positions, opt):
+    return apply_transform(params, vertex_positions, opt['rotation'], opt['translation'])
+
+# display a histogram and three images in a figure
 def display_results(loss_hist, img_init, img_final, img_ref):
     fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 
-    # Display the loss histogram in the top left
+    # display the loss histogram in the top left
     axs[0][0].plot(loss_hist)
     axs[0][0].set_xlabel('Iteration')
     axs[0][0].set_ylabel('Error')
     axs[0][0].set_title('Image loss over iteration')
 
-    # Display the initialization image in the top right
+    # display the initialization image in the top right
     axs[0][1].imshow(img_init)
     axs[0][1].axis('off')
     axs[0][1].set_title('Initial image')
 
-    # Display the optimized result in the bottom left
+    # display the optimized result in the bottom left
     axs[1][0].imshow(img_final)
     axs[1][0].axis('off')
     axs[1][0].set_title('Optimized result')
 
-    # Display the reference image in the bottom right
+    # display the reference image in the bottom right
     axs[1][1].imshow(img_ref)
     axs[1][1].axis('off')
     axs[1][1].set_title('Reference image')
