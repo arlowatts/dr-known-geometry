25d8f23ee0448e0bf26d796e9b0c786cadd12370
Arlo Watts <arlowatts1@gmail.com>
2025-04-08 18:35:26 -0700

    Convert render_templates.py to a class

--- testing/full_pipeline/render_templates.py
+++ testing/full_pipeline/render_templates.py
@@ -1,7 +1,7 @@
 import mitsuba as mi
 import drjit as dr
 import numpy as np
-import sys, os, math, random
+import os, math, random
 
 import loader
 
@@ -10,6 +10,60 @@ img_binary_threshold = 0.5
 
 # set the initial learning rate for the Adam optimizers
 learning_rate = 0.025
+
+# define reasonable defaults for the sensor parameters
+default_sensor_fov = 80
+default_sensor_ppo = (0, 0)
+default_sensor_distance = 1
+
+class PoseEstimator:
+    """Estimate the pose of the camera relative to the object in the binary reference images."""
+
+    def __init__(self, model_path: str, model_type: str, ref_dir: str, ref_shape: (int, int), tmplt_count: int, tmplt_shape: (int, int), opt_iters: int):
+
+        # check arguments
+        if not os.path.isfile(model_path):             raise ValueError('model_path must be an existing file')
+        if not model_type in ('obj', 'ply'):           raise ValueError('model_type must be "obj" or "ply"')
+        if not os.path.isdir(ref_dir):                 raise ValueError('ref_dir must be an existing directory')
+        if ref_shape[0] <= 0 or ref_shape[1] <= 0:     raise ValueError('ref_shape must be greater than (0, 0)')
+        if tmplt_count <= 0:                           raise ValueError('tmplt_count must be greater than 0')
+        if tmplt_shape[0] <= 0 or tmplt_shape[1] <= 0: raise ValueError('tmplt_shape must be greater than (0, 0)')
+        if opt_iters <= 0:                             raise ValueError('opt_iters must be greater than 0')
+
+        self.model_path = model_path
+        self.model_type = model_type
+        self.ref_dir = ref_dir
+        self.ref_shape = ref_shape
+        self.tmplt_count = tmplt_count
+        self.tmplt_shape = tmplt_shape
+        self.opt_iters = opt_iters
+        self.refs = None
+        self.poses = None
+        self.opt_transforms = None
+
+        # load a scene with the model and a non-differentiable integrator
+        print('Loading template scene')
+        tmplt_scene = mi.load_dict(loader.get_scene_dict(self.model_path, self.model_type))
+
+        # load the reference images
+        print('Loading reference images')
+        self.refs = loader.get_refs(self.ref_dir, self.ref_shape, img_binary_threshold)
+
+        # render a set of templates
+        tmplts = render_tmplts(tmplt_scene, self.tmplt_count, self.tmplt_shape)
+
+        # match the templates to the reference images
+        self.poses = match_poses(tmplt_scene, tmplts, self.refs)
+
+    def optimize(self):
+
+        # reload the scene with a differentiable integrator
+        print('Loading optimization scene')
+        sensor_dicts = [loader.get_sensor_dict(sensor_params, self.ref_shape) for sensor_params in self.poses]
+        opt_scene = mi.load_dict(loader.get_scene_dict(self.model_path, self.model_type, sensor_dicts=sensor_dicts, differentiable=True))
+
+        # optimize the poses for each reference image
+        self.opt_transforms = optimize_poses(opt_scene, self.refs, self.opt_iters)
 
 def render_tmplts(scene: 'mi.Scene', tmplt_count: int, tmplt_shape: (int, int)) -> list[tuple[tuple['mi.ScalarTransform4f',float,tuple[float,float],float],tuple[tuple[float,float],tuple[float,float],float]]]:
     """Render silhouette templates of a given scene.
@@ -22,17 +76,17 @@ def render_tmplts(scene: 'mi.Scene', tmplt_count: int, tmplt_shape: (int, int)) 
 
     for i in range(tmplt_count):
 
+        # set the default sensor parameters
+        sensor_fov = default_sensor_fov
+        sensor_ppo = default_sensor_ppo
+        sensor_distance = default_sensor_distance
+
         # generate a uniformly distributed random point on the unit sphere
         square_point = mi.Point2f(random.random(), random.random())
-        sphere_point = mi.warp.square_to_uniform_sphere(square_point)
+        sphere_point = sensor_distance * mi.warp.square_to_uniform_sphere(square_point)
 
         # position the sensor on the unit sphere looking at the origin
         sensor_to_world = mi.Transform4f().look_at(sphere_point, (0, 0, 0), (1, 0, 0))
-
-        # set other sensor parameters to reasonable defaults
-        sensor_fov = 60
-        sensor_ppo = (0, 0)
-        sensor_distance = 1
 
         # load the sensor
         sensor_params = (sensor_to_world, sensor_fov, sensor_ppo, sensor_distance)
@@ -131,7 +185,7 @@ def optimize_poses(scene: 'mi.Scene', refs: list[tuple['mi.TensorXf',tuple[tuple
     Optimize the position, orientation, and field of view of each sensor.
     """
 
-    opt_poses = []
+    opt_transforms = []
 
     # access the scene parameters, including all of the sensors
     params = mi.traverse(scene)
@@ -178,80 +232,6 @@ def optimize_poses(scene: 'mi.Scene', refs: list[tuple['mi.TensorXf',tuple[tuple
         print()
 
         # update the sensor with the optimized pose
-        opt_poses.append(transform.inverse() @ params[f'sensor{i}.to_world'])
+        opt_transforms.append(transform.inverse() @ params[f'sensor{i}.to_world'])
 
-    return opt_poses
-
-def main():
-    """Determine approximate poses with arguments parsed from the command line."""
-
-    if len(sys.argv) < 8:
-        print('USAGE:', 'python', sys.argv[0], 'MODEL_PATH', 'MODEL_TYPE', 'TEMPLATE_COUNT', 'TEMPLATE_SIZE', 'REFERENCE_DIRECTORY', 'REFERENCE_SIZE', 'OPTIMIZATION_ITERATIONS')
-        return
-
-    model_path = sys.argv[1]
-    model_type = sys.argv[2]
-    tmplt_count = int(sys.argv[3])
-    tmplt_size = int(sys.argv[4])
-    ref_dir = sys.argv[5]
-    ref_size = int(sys.argv[6])
-    opt_iters = int(sys.argv[7])
-
-    if not os.path.isfile(model_path):
-        print('MODEL_PATH must be an existing file')
-        return
-
-    if not model_type in ('obj', 'ply'):
-        print('MODEL_TYPE must be "obj" or "ply"')
-        return
-
-    if tmplt_count <= 0:
-        print('TEMPLATE_COUNT must be greater than 0')
-        return
-
-    if tmplt_size <= 0:
-        print('TEMPLATE_SIZE must be greater than 0')
-        return
-
-    if not os.path.isdir(ref_dir):
-        print('REFERENCE_DIRECTORY must be an existing directory')
-        return
-
-    if ref_size <= 0:
-        print('REFERENCE_SIZE must be greater than 0')
-        return
-
-    if opt_iters <= 0:
-        print('OPTIMIZATION_ITERATIONS must be greater than 0')
-        return
-
-    # set a non-differentiable variant for template rendering
-    mi.set_variant('scalar_rgb')
-
-    # load the scene with the given model and non-differentiable integrator
-    print('Loading template scene')
-    scene = mi.load_dict(loader.get_scene_dict(model_path, model_type))
-
-    # load the reference images
-    print('Loading reference images')
-    refs = loader.get_refs(ref_dir, (ref_size, ref_size), img_binary_threshold)
-
-    # render a set of templates
-    tmplts = render_tmplts(scene, tmplt_count, (tmplt_size, tmplt_size))
-
-    # match the templates to the reference images
-    poses = match_poses(scene, tmplts, refs)
-
-    # set a differentiable variant for pose optimization
-    try: mi.set_variant('cuda_ad_mono')
-    except: mi.set_variant('llvm_ad_mono')
-
-    # reload the scene with a differentiable integrator
-    print('Loading optimization scene')
-    sensor_dicts = [loader.get_sensor_dict(sensor_params, (ref_size, ref_size)) for sensor_params in poses]
-    scene = mi.load_dict(loader.get_scene_dict(model_path, model_type, sensor_dicts=sensor_dicts, differentiable=True))
-
-    # optimize the poses for each reference image
-    opt_poses = optimize_poses(scene, refs, opt_iters)
-
-if __name__ == '__main__': main()
+    return opt_transforms
